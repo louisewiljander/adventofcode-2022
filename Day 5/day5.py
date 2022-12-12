f93a72ad3b35019318b0cf97da566e7552a5b03d
Louise Wiljander <lwiljander@microsoft.com>
2022-12-12 15:50:54 +0100

    Created files for day 5

---
+++ Day 5/day5.py
@@ -0,0 +1,10 @@
+#------  DATA INPUT ZONE ------#
+input_file = open(r"C:\Users\lwiljander\VS Code Lab\adventofcode-2022\adventofcode-2022\Day 5\input.txt", "r")
+test_file = open(r"C:\Users\lwiljander\VS Code Lab\adventofcode-2022\adventofcode-2022\Day 5\test.txt", "r")
+
+assignment_pairs = input_file.read().splitlines()
+test_pairs = test_file.read().splitlines()
+
+######################## Task 1 ########################
+
+#------ RUNNING SOLUTION ------#
